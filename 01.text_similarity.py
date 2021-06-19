e637e7dc905a78762eaf018b91bbf1c01c5dc332
kobe24o <mingmingchen1989@163.com>
2021-06-19 19:21:20 +0800

    Update 01.text_similarity.py

--- 01.text_similarity.py
+++ 01.text_similarity.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 
 import time
 import os
@@ -7,14 +7,14 @@ import paddle
 import paddlenlp
 import paddle.nn.functional as F
 from paddlenlp.datasets import load_dataset
-import paddle.distributed as dist #并行
+import paddle.distributed as dist  # 并行
 
 if __name__ == "__main__":
     dist.init_parallel_env()  # 初始化并行环境
     # 启动命令 python -m paddle.distributed.launch --gpus '0,1' xxx.py &
-    batch_size=64
+    batch_size = 64
     epochs = 5
-    
+
     # 加载数据集
     train_ds, dev_ds = load_dataset("lcqmc", splits=["train", "dev"])
     # 展示数据
@@ -27,10 +27,13 @@ if __name__ == "__main__":
         # {'query': '求秋色之空漫画全集', 'title': '求秋色之空全集漫画', 'label': 1}
         # {'query': '晚上睡觉带着耳机听音乐有什么害处吗？', 'title': '孕妇可以戴耳机听音乐吗?', 'label': 0}
 
-    #%%
+    # %%
     # 数据预处理
+    # 使用预训练模型的tokenizer
     tokenizer = paddlenlp.transformers.ErnieGramTokenizer.from_pretrained("ernie-gram-zh")
     # https://gitee.com/paddlepaddle/PaddleNLP/blob/develop/docs/model_zoo/transformers.rst
+
+
     def convert_data(data, tokenizer, max_seq_len=512, is_test=False):
         text1, text2 = data["query"], data["title"]
         encoded_inputs = tokenizer(text=text1, text_pair=text2, max_seq_len=max_seq_len)
@@ -42,20 +45,26 @@ if __name__ == "__main__":
             return input_ids, token_type_ids, label
         return input_ids, token_type_ids
 
+
     input_ids, token_type_ids, label = convert_data(train_ds[0], tokenizer)
     print(input_ids)
+    # [1, 692, 811, 445, 2001, 497, 5, 654, 21, 692, 811, 614, 356, 314, 5, 291, 21, 2, 329, 445, 2001, 497, 5, 654, 21, 692, 811, 614, 356, 314, 5, 291, 21, 2]
     print(token_type_ids)
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     print(label)
+    # [1]
 
     from functools import partial
+
     trans_func = partial(convert_data, tokenizer=tokenizer, max_seq_len=512)
 
     from paddlenlp.data import Stack, Pad, Tuple
-    batchify_fn = lambda samples, fn = Tuple(
+
+    batchify_fn = lambda samples, fn=Tuple(
         Pad(axis=0, pad_val=tokenizer.pad_token_id),
         Pad(axis=0, pad_val=tokenizer.pad_token_type_id),
         Stack(dtype="int64")
-    ) : [d for d in fn(samples)]
+    ): [d for d in fn(samples)]
     # 将长度不同的多个句子padding到统一长度，取N个输入数据中的最大长度
     # 长度是指的： 一个batch中的最大长度，主要考虑性能开销
 
@@ -76,10 +85,13 @@ if __name__ == "__main__":
         return_list=True
     )
 
-    #%%
+    # %%
     import paddle.nn as nn
+
     pretrained_model = paddlenlp.transformers.ErnieGramModel.from_pretrained("ernie-gram-zh")
-    #%%
+
+
+    # %%
 
     class TeachingPlanModel(nn.Layer):
         def __init__(self, pretrained_model, dropout=None):
@@ -87,6 +99,7 @@ if __name__ == "__main__":
             self.ptm = pretrained_model
             self.dropout = nn.Dropout(dropout if dropout is not None else 0.1)
             self.clf = nn.Linear(self.ptm.config["hidden_size"], 2)
+
         def forward(self, input_ids, token_type_ids=None, position_ids=None, attention_mask=None):
             _, cls_embedding = self.ptm(input_ids, token_type_ids, position_ids, attention_mask)
             cls_embedding = self.dropout(cls_embedding)
@@ -94,15 +107,17 @@ if __name__ == "__main__":
             probs = F.softmax(logits)
             return probs
 
+
     model = TeachingPlanModel(pretrained_model)
     # 并行训练设置
     # https://aistudio.baidu.com/aistudio/projectdetail/1222066
     # https://www.paddlepaddle.org.cn/documentation/docs/zh/guides/02_paddle2.0_develop/06_device_cn.html
 
-    #%%
+    # %%
     # 训练评估
     from paddlenlp.transformers import LinearDecayWithWarmup
-    num_training_steps = len(train_data_loader)*epochs
+
+    num_training_steps = len(train_data_loader) * epochs
     lr_scheduler = LinearDecayWithWarmup(5e-5, num_training_steps, 0.0)
     decay_params = [
         p.name for n, p in model.named_parameters()
@@ -112,13 +127,15 @@ if __name__ == "__main__":
         learning_rate=lr_scheduler,
         parameters=model.parameters(),
         weight_decay=0.0,
-        apply_decay_param_fun=lambda x : x in decay_params
+        apply_decay_param_fun=lambda x: x in decay_params
     )
 
     criterion = paddle.nn.loss.CrossEntropyLoss()
 
     metric = paddle.metric.Accuracy()
-    #%%
+
+
+    # %%
     @paddle.no_grad()
     def evaluate(model, criterion, metric, data_loader, phase="dev"):
         model.eval()
@@ -136,10 +153,11 @@ if __name__ == "__main__":
         model.train()
         metric.reset()
 
+
     global global_step
     global_step = 0
     t_start = time.time()
-    for epoch in range(1, epochs+1):
+    for epoch in range(1, epochs + 1):
         for step, batch in enumerate(train_data_loader, start=1):
             input_ids, token_type_ids, labels = batch
             probs = model(input_ids=input_ids, token_type_ids=token_type_ids)
@@ -151,29 +169,31 @@ if __name__ == "__main__":
             # if global_step > 10 :
             #     break # 调试代码
 
-            if global_step%10 == 0:
+            if global_step % 10 == 0:
                 print("训练步数 %d, epoch: %d, batch: %d, loss: %.5f, acc: %.5f, speed: %.2f step/s"
-                    % (global_step, epoch, step, loss, acc,
-                        10 / (time.time() - t_start)))
+                      % (global_step, epoch, step, loss, acc,
+                         10 / (time.time() - t_start)))
                 t_start = time.time()
             loss.backward()
             optimizer.step()
             lr_scheduler.step()
             optimizer.clear_grad()
 
-            if global_step%100 == 0:
+            if global_step % 100 == 0:
                 evaluate(model, criterion, metric, dev_data_loader, "dev")
-    #%%
+    # %%
     pathname = "checkpoint"
-    isExists=os.path.exists(pathname)
+    isExists = os.path.exists(pathname)
     if not isExists:
         os.mkdir(pathname)
     save_dir = os.path.join(pathname, "model_%d" % global_step)
     save_param_path = os.path.join(save_dir, "model_state_pdparams")
     paddle.save(model.state_dict(), save_param_path)
     tokenizer.save_pretrained(save_dir)
-    #%%
-    #预测
+
+
+    # %%
+    # 预测
     def predict(model, data_loader):
         batch_probs = []
         model.eval()
@@ -188,16 +208,17 @@ if __name__ == "__main__":
             batch_probs = np.concatenate(batch_probs, axis=0)
             return batch_probs
 
+
     trans_func_test = partial(convert_data, tokenizer=tokenizer, max_seq_len=512, is_test=True)
 
-    batchify_fn = lambda samples, fn = Tuple(
+    batchify_fn = lambda samples, fn=Tuple(
         Pad(axis=0, pad_val=tokenizer.pad_token_id),
         Pad(axis=0, pad_val=tokenizer.pad_token_type_id)
-    ) : [data for data in fn(samples)]
+    ): [data for data in fn(samples)]
 
     test_ds = load_dataset("lcqmc", splits=["test"])
 
-    batch_sampler = paddle.io.BatchSampler(test_ds,batch_size=batch_size, shuffle=False)
+    batch_sampler = paddle.io.BatchSampler(test_ds, batch_size=batch_size, shuffle=False)
     predict_data_loader = paddle.io.DataLoader(
         dataset=test_ds.map(trans_func_test),
         batch_sampler=batch_sampler,
@@ -205,7 +226,7 @@ if __name__ == "__main__":
         return_list=True
     )
 
-    #%%
+    # %%
     pretrained_model = paddlenlp.transformers.ErnieGramModel.from_pretrained("ernie-gram-zh")
     model = TeachingPlanModel(pretrained_model)
     state_dict = paddle.load(save_param_path)
